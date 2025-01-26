5325e57194df05b321eeee4af284ac2a5783f2b8
Heckbit <jacobmgoudy@gmail.com>
2025-01-25 23:47:08 -0500

    Added file renaming in the specified directory

--- movie-search.py
+++ movie-search.py
@@ -6,17 +6,24 @@ imdbpy = imdb.IMDb()
 
 files = os.listdir(file_path)
 
-# movie = imdbpy.get_movie('0372784')
-# print(movie)
-
 for file in files:
-  movie_name = file.split(".")[0]
-  extension = file.split(".")[1]
-  
-  movie_result = imdbpy.search_movie(movie_name)
-  imdb_id = movie_result[0].getID()
-  movie = imdbpy.get_movie(imdb_id)
+  if file.find("imdb") == -1:
+    movie_name = file.split(".")[0]
+    extension = file.split(".")[1]
 
-  new_name = f"{movie.get('title')} ({movie.get('year')}) \u007bimdb-tt{imdb_id}\u007d.{extension}"
+    movie_result = imdbpy.search_movie(movie_name)
+    imdb_id = movie_result[0].getID()
+    movie = imdbpy.get_movie(imdb_id)
 
-  print(new_name)
+    new_name = f"{movie.get('title')} ({movie.get('year')}) \u007bimdb-tt{imdb_id}\u007d.{extension}"
+    new_name = new_name.replace(":","")
+    new_name = new_name.replace("?","")
+
+    print(f"Current: {file}")
+    print(f"New: {new_name}")
+
+    old_path = file_path + "\\" + file
+    new_path = file_path + "\\" + new_name
+
+    os.rename(old_path, new_path)
+
